4787e363d71522c6a6a4f51febd0f3955d65df5b
Strada2000 <sanketmense@gmail.com>
2021-02-21 18:37:36 +0530

    init

---
+++ build/precache-manifest.b9668aed5c3b69fcdc9e404d9662876f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f3aefa5f13c7e1e6615",
+    "url": "/Calculator/static/css/main.eb626c26.chunk.css"
+  },
+  {
+    "revision": "9f3aefa5f13c7e1e6615",
+    "url": "/Calculator/static/js/main.38a89cca.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "9cdeaca24e521464c3e9",
+    "url": "/Calculator/static/js/2.f63c8625.chunk.js"
+  },
+  {
+    "revision": "b38806056b5bdc808a60074b1e703bd9",
+    "url": "/Calculator/index.html"
+  }
+];
